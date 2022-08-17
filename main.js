aa9ca95ebba17aa26efeb8903f3ad9eb8800fd1d
walber2903 <walber.menezes@hotmail.com>
2022-08-17 15:03:11 -0300

    Last commit

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
+//Events
+const screen1 = document.querySelector(".screen1")
+const screen2 = document.querySelector(".screen2")
+const btnTry = document.querySelector("#btnTry");
+const btnReset = document.querySelector("#btnReset");
+
 const randomNumber = Math.round(Math.random() * 10);
 
 let attempts = 1;
 
-function handleClick(event) {
+//Events
+btnTry.addEventListener('click', handleTryClick);
+btnReset.addEventListener('click', handleResetClick);
+
+//Callbacks functions
+function handleTryClick(event) {
     event.preventDefault();
 
     const inputNumber = document.querySelector("#inputNumber");
@@ -12,11 +23,22 @@ function handleClick(event) {
     typeof(randomNumber)
 
     if(Number(inputNumber.value) == randomNumber) {
-        document.querySelector(".screen1").classList.add("hide");
-        document.querySelector(".screen2").classList.remove("hide");
-
-        console.log(`Voce acertou em ${attempts} tentativas! O numero escolhido era ${randomNumber}`)
+        screen1.classList.add("hide");
+        screen2.classList.remove("hide");
+        
+        document
+            .querySelector(".screen2 h2")
+            .innerText = `Voce acertou em ${attempts} tentativas! O número escolhido era ${randomNumber}`
     }
+
+    inputNumber.value = "";
 
     attempts++;
 }
+
+function handleResetClick() {
+    screen1.classList.remove("hide");
+    screen2.classList.add("hide");
+    attempts = 1;
+}
+
